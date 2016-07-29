76fc3929b7f027c4fe5e94e4ec6b2b86ae336cfd
Maya Saxena <maya@intrepid.io>
2016-07-29 13:50:03 -0400

    Add Xcode project

---
+++ Corner Judge Trainer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Corner Judge Trainer
+//
+//  Created by Maya Saxena on 7/29/16.
+//  Copyright © 2016 Maya Saxena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
